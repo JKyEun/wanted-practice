279c5fa2c665254046379bfa6d5b0d28d4beba6c
JKyEun <j56237@naver.com>
2023-05-01 00:58:27 +0900

    test commit5

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function Test() {
-  const arr = [1, 2, 3];
-  const str = 'ar';
-  var str = '';
-  console.log(arr);
   return <div>Hello, World</div>;
 }
