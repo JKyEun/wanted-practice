e9493d0e668d1840f37b56ec22d2cd08e9e75259
JKyEun <j56237@naver.com>
2023-05-05 22:09:15 +0900

    CICD 완성

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Test() {
-  return <h2>Hello, World</h2>;
+  return (
+    <>
+      <h2>Hello, World</h2>
+      <div>안녕 배포야</div>
+    </>
+  );
 }
