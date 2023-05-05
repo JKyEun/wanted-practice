5178a90d9d65c55068430558a50fc05e75190b41
JKyEun <j56237@naver.com>
2023-05-05 21:09:12 +0900

    배포 CICD용 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Test from './components/Test';
 
 function App() {
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
 export default function Test() {
-  const arr = [1, 3, 2];
-  return <div>Hello, World</div>;
+  return <h2>Hello, World</h2>;
 }
